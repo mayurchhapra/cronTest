40100928db8b2e1d7961fba4e8ab08c48a1cdd30
Mayur Chhapra <mayurchhapra@gmail.com>
2018-09-07 19:52:15 +0530

    Change in loop.

--- cronjobPosibilityFinding.js
+++ cronjobPosibilityFinding.js
@@ -1,8 +1,8 @@
 let date = new Date();
 let pattern = [];
 let default_pattern = "* * * * *";
-
-for (let i=1; i<=6; i++) {
+pattern.push(default_pattern);
+for (let i=1; i<=5; i++) {
 	switch (i) {
 		case 1:
 			console.log(i);
@@ -24,15 +24,15 @@ for (let i=1; i<=6; i++) {
 			console.log(i);
 			pattern.push(loop(i, day(), default_pattern));
 			break;
-		case 6:
-			console.log(i);
-			break;
+		// case 6:
+		// 	console.log(i);
+		// 	break;
 		default:
 			console.log('Invalid');
 			break;
 	}
 }
-
+console.log("----------------------");
 console.log(pattern);
 
 function min() {
@@ -48,7 +48,7 @@ function sys_date() {
 }
 
 function month() {
-	return date.getMonth()+1
+	return date.getMonth()+1;
 }
 
 function day() {
@@ -57,12 +57,31 @@ function day() {
 
 function loop(i, patternValue, default_pattern) {
 	let array=[];
-	for (let j=0; j<=i; j++) {
-		if( j==0 ) 
-			array.push(default_pattern);
-		
-		else
-			array.push(patternValue.toString()+" * * * *");
+	// for (let j=0; j<=i; j++) {
+	// 	if( j==0 ) 
+	// 		array.push(default_pattern);
+	// 	else {
+			switch (i) {
+				case 1:
+					array.push(patternValue.toString()+" * * * *");
+					break;
+				case 2:
+				array.push("* "+patternValue.toString()+" * * *");
+					break;
+				case 3:
+					array.push("* * "+patternValue.toString()+" * *");
+					break;
+				case 4:
+					array.push("* * * "+patternValue.toString()+" *");
+					break;
+				case 5:
+					array.push("* * * * "+patternValue.toString());
+					break;
+				
+				default: 
+					console.log('Out of Range');
+		// 	}
+		// }
 		console.log('\t',j, '\t : ', patternValue);
 	}
 	console.log(array);
